8d15e1c23d6b47082344369e17e6379b7c33b4fd
mauro_larese <mauro.larese@omnys.com>
2025-02-04 12:02:01 +0100

    Implement MenuRunner with dependency injection and logging

--- src/main/java/it/epicode/pizzeria_vesuvio/runners/MenuRunner.java
+++ src/main/java/it/epicode/pizzeria_vesuvio/runners/MenuRunner.java
@@ -2,5 +2,29 @@ package it.epicode.pizzeria_vesuvio.runners;
 
 
 // questo runner utilizza la dependency injection tramite costruttore
-public class MenuRunner {
+
+import it.epicode.pizzeria_vesuvio.entity.Pizza;
+import it.epicode.pizzeria_vesuvio.stampe.PrintService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+
+@RequiredArgsConstructor
+public class MenuRunner implements CommandLineRunner {
+    private final PrintService printService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Pizza pizza = new Pizza();
+        pizza.setNome("Margherita");
+        pizza.setPrezzo(5.0);
+        pizza.setCalorie(500);
+
+        log.info("---- stampa con dependency injection tramite costruttore");
+        printService.stampa(pizza);
+    }
 }
